f2c67366800c80b60628dc9521e8cb773e85a663
ravilock <ravi.me.professional@hotmail.com>
2025-03-14 01:10:40 -0300

    fix: global feed pagination

--- src/routes/+page.js
+++ src/routes/+page.js
@@ -3,22 +3,29 @@ import { error } from "@sveltejs/kit";
 
 export const ssr = false
 
+const listArticlesPageLimit = 20;
+
 /** @type {import('./$types').PageLoad} */
-export async function load() {
-  const [articles] = await Promise.all([loadArticles()]);
+export async function load({ url }) {
+  const page = Number(url.searchParams.get("page") || 1) - 1;
+  const offset = page * listArticlesPageLimit;
+  const [articles] = await Promise.all([loadArticles(offset)]);
   return {
     articles: articles,
   }
 }
 
-async function loadArticles() {
+/**
+ * @param {number} offset
+ */
+async function loadArticles(offset) {
   const token = getToken();
   const headers = new Headers({
     "Content-Type": "application/json",
   });
   if (token) headers.set("Authorization", `Bearer ${token}`);
   const response = await fetch(
-    `${import.meta.env.VITE_API_URL}/api/articles`,
+    `${import.meta.env.VITE_API_URL}/api/articles?offset=${offset}`,
     { headers },
   );
   const data = await response.json();
